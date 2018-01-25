50c8f38fdff88028ab8d793ae86ac587990d95d6
The EO <edeesonopinav4@gmail.com>
2018-01-25 21:25:46 +0800

    Added test email

--- resources/views/my_profile.blade.php
+++ resources/views/my_profile.blade.php
@@ -127,7 +127,7 @@ use App\Verification;
 						</div>
 
 						<div class="col-md-4">
-							<a href="{{ url(Auth::user()->type) }}" download>
+							<a href="{{ url(Auth::user()->type) }}">
 							<input type="button" class="btn btn-primary" value="VIEW CV / PORTFOLIO" style="color: #333;background:#30c3e7;border: 1px solid #30c3e7;">
 							</a>
 
