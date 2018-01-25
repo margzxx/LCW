27b988aaf5d04a0e5a8e15f1242c72714d9ad10e
The EO <edeesonopinav4@gmail.com>
2018-01-25 23:47:03 +0800

    Fixed some bugs

--- resources/views/my_profile.blade.php
+++ resources/views/my_profile.blade.php
@@ -127,7 +127,7 @@ use App\Verification;
 						</div>
 
 						<div class="col-md-4">
-							<a href="{{ url(File::where('type','Resume')->where('user_id',$user->id)) }}">
+							<a href="{{ url(File::where('type','Resume')->where('user_id',Auth::user()->id)->first()->document) }}">
 							<input type="button" class="btn btn-primary" value="VIEW CV / PORTFOLIO" style="color: #333;background:#30c3e7;border: 1px solid #30c3e7;">
 							</a>
 
