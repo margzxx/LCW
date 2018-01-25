9f56db8cf5285ba143efd4837672370c17a1e1c0
The EO <edeesonopinav4@gmail.com>
2018-01-25 19:00:24 +0800

    Added test email

--- resources/views/emails/success_register.blade.php
+++ resources/views/emails/success_register.blade.php
@@ -7,9 +7,19 @@
 
 	<h3>Welcome to Connected Women</h3>
 
-	Hi <b>{{ $name }},</b>
+	Hi <b>{{ $name }}</b>,
+	<br>
+	Thank you for signing up with Connected Women - Virtual Assistants For Entrepreneurs!<br>
+	To activate your account , please click the link below to confirm your email address:<br><br>
 
-	<a href="{{ url('activate-account/'.$user_id) }}"></a>
+	<a href="{{ url('http://phplaravel-135925-393147.cloudwaysapps.com/login'.$user_id) }}">http://phplaravel-135925-393147.cloudwaysapps.com/activate-account</a>
+
+	<br><br>
+	If you have any problems, please contact us at cwjobs@jobs.connectedwomen.co
+	<br>
+	Thanks,
+	<br>
+	Connected Women - Virtual Assistants For Entrepreneurs
 
 </body>
 </html>
