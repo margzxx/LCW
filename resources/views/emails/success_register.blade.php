433537bf7236c41cc02e7ad62c9936cfa76a61d8
The EO <edeesonopinav4@gmail.com>
2018-01-25 21:28:46 +0800

    Added test email

--- resources/views/emails/success_register.blade.php
+++ resources/views/emails/success_register.blade.php
@@ -7,7 +7,7 @@
 
 	<h3>Welcome to Connected Women</h3>
 
-	Hi <b>{{ $name }}</b>,
+	Hi <b>{{ $name }}</b>,s
 	<br>
 	Thank you for signing up with Connected Women - Virtual Assistants For Entrepreneurs!<br>
 	To activate your account , please click the link below to confirm your email address:<br><br>
